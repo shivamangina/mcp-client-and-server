b840ec9281f74dfc2cb2dd9b05771b251d409442
Shiva Kumar Mangina <shiva2nani.mangina@gmail.com>
2025-06-25 20:49:42 +0530

    fix: Add chat ui

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,18 +1,41 @@
-import React from 'react'
-import { Textarea } from './ui/textarea'
+"use client";
+import React from "react";
+import { Textarea } from "./ui/textarea";
 
 export default function Chat() {
   return (
     <div className="flex flex-col h-full  items-center justify-between p-6">
-    <div className="flex flex-col">
-      <span className="font-semibold">chat</span>
+      <div className="flex flex-col">
+        <span className="font-semibold">chat</span>
+      </div>
+
+       {/* Chat Messages */}
+       <div className="flex flex-col h-full justify-end gap-2 overflow-y-auto w-full">
+        <span className="flex flex-col items-start gap-2  border-gray-200 p-4 w-full">
+          Who is the current president of the United States?
+          </span>
+
+          <span className="flex flex-col items-start gap-2  border-gray-200 p-4 w-full">
+          Who is the current president of the United States?
+          </span>
+      </div>
+
+
+     
+      <div className="grid w-full gap-3">
+        <Textarea placeholder="Type your message here." id="message-2" />
+        <p className="text-muted-foreground text-sm">
+          Your message will be copied to the support team.
+        </p>
+        <button
+          onClick={() => {
+            console.log("send");
+          }}
+          className="h-10 rounded-lg border border-gray-200 p-2 hover:bg-gray-200"
+        >
+          Send
+        </button>
+      </div>
     </div>
-    <div className="grid w-full gap-3">
-      <Textarea placeholder="Type your message here." id="message-2" />
-      <p className="text-muted-foreground text-sm">
-        Your message will be copied to the support team.
-      </p>
-    </div>
-  </div>
-  )
+  );
 }
--- components/Tools.tsx
+++ components/Tools.tsx
@@ -2,8 +2,22 @@ import React from "react";
 
 export default function Tools() {
   return (
-    <div className="flex h-full items-center justify-center p-6">
+    <div className="flex h-full flex-col gap-2 p-6">
       <span className="font-semibold">Available Tools</span>
+      <div className="flex flex-col gap-2">Add</div>
+
+      {/* Tool Invocations */}
+      <div className="flex flex-col gap-2 ">
+        <span className="flex flex-col gap-2 rounded-lg border border-gray-200 p-4">
+          <span className="font-semibold">Tool 1</span>
+          <span className="text-sm text-gray-500">Tool 1 description</span>
+        </span>
+
+        <span className="flex flex-col gap-2 rounded-lg border border-gray-200 p-4">
+          <span className="font-semibold">Tool 2</span>
+          <span className="text-sm text-gray-500">Tool 2 description</span>
+        </span>
+      </div>
     </div>
   );
 }
